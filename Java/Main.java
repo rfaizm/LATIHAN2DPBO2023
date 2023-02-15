4e37e435d1f2ac4502baaeda551514217205b7ec
rfaizm <rachmanfaiz02@gmail.com>
2023-02-15 11:07:38 +0700

    Latihan2

--- Java/Main.java
+++ Java/Main.java
@@ -1,7 +1,12 @@
+import java.util.ArrayList;
+
 class Main{
     public static void main(String[] args){
+        int n = 0;
+        int i = 0;
+        // Data Dummy
         Mahasiswa aku = new Mahasiswa();
-
+        Mahasiswa dia = new Mahasiswa();
         aku.setNik("321.120.777");
         aku.setName("Rachman");
         aku.setGender('L');
@@ -11,16 +16,24 @@ class Main{
         aku.setProdi("IK");
         aku.setFakultas("FPMIPA");
 
-        System.out.println("NIK : " + aku.getNik());
-        System.out.println("Nama : " + aku.getName());
-        System.out.println("Gender : " + aku.getGender());
-        System.out.println("Kampus : " + aku.getKampus());
-        System.out.println("Email : " + aku.getEmail());
-        System.out.println("NIM : " + aku.getNIM());
-        System.out.println("Program Studi : " + aku.getProdi());
-        System.out.println("Fakultas : " + aku.getFakultas());
-        aku.eat();
-        aku.sleep();
+        dia.setNik("321.100.890");
+        dia.setName("Dia_Sayang");
+        dia.setGender('P');
+        dia.setKampus("UNI");
+        dia.setEmail("unknown@uni.edu");
+        dia.setNIM("2178090");
+        dia.setProdi("IK");
+        dia.setFakultas("FPMIPA");
+        //Buat array list
+        ArrayList<Mahasiswa> list = new ArrayList<>();
+        //Masukkan data dummy ke dalam list
+        list.add(aku);
+        list.add(dia);
+        // Output
+        System.out.println("Daftar Human : ");
+        for(i=0; i<list.size(); i++){
+            System.out.println(Integer.toString(i+1) + ". " + list.get(i).getNik() + " | " + list.get(i).getName() + " | " + list.get(i).getGender() + " | " + list.get(i).getKampus() + " | " + list.get(i).getEmail() + " | " + list.get(i).getNIM() + " | " + list.get(i).getProdi() + " | " + list.get(i).getFakultas());
+        }
 
     }
 }
